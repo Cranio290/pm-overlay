79c5f30ca8d342fb83ff8410cf9bbb89a912eb2c
Cranio290 <143084676+Cranio290@users.noreply.github.com>
2023-10-07 21:58:27 +0200

    Update overlay.js

--- overlay/overlay.js
+++ overlay/overlay.js
@@ -1,5 +1,12 @@
 //Box
-var html =  '<div style="background-color:white; border:3px solid black; border-radius: 5px; padding:5px; position:absolute; top: 10px; right: 10px;">Overlay:<br>X:<input type="text" id="x"></input></div>'
+const html = `<div style="background-color:white; border:3px solid black; border-radius: 5px; padding:5px; position:absolute; top: 10px; right: 10px;">
+<b>Overlay:</b><br>
+Template: <input type="text" id="link"></input><input type="checkbox"><br>
+<input type="file"><br>
+X: <input type="text" id="x"></input><br>
+Y: <input type="text" id="y"></input>
+</div>`;
+
 const body = document.getElementsByTagName("body")[0];
 const div = document.createElement("div");
 div.innerHTML = html;
@@ -9,7 +16,9 @@ body.append(div);
 function render() {
   const canvas = document.getElementsByClassName("viewport")[0];
   const ctx = canvas.getContext("2d");
+  var x = window.location.hash;
+  var y = 
   ctx.fillStyle = "red";
-  ctx.fillRect(20, 10, 150, 100);
+  ctx.fillRect(10, 10, 150, );
 }
 setInterval(render, 10);
